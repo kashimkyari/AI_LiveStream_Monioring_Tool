a234203e01bd3d5e4ffdc5dc4966354cc36571b4
Kashim Kyari Mohammed <kashimkyari@gmail.com>
2025-03-07 03:51:21 +0100

    fix k8s

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Start the React frontend (runs on port 3000)
-cd /app/frontend && npm start &
-
-# Optionally, wait a few seconds for the frontend to initialize
-sleep 5
-
-# Start the Flask backend (runs on port 5000)
-cd /app/backend && python app.py
